60b9605259b2e3fbddd24ffe80865bf56f7f8aa7
Vedang Manerikar <ved.manerikar@gmail.com>
2013-09-21 18:48:10 +0530

    Initial commit on the Little Cljer

---
+++ little_cljer/project.clj
@@ -0,0 +1,6 @@
+(defproject little_cljer "0.1.0-SNAPSHOT"
+  :description "The Little Schemer - written in clojure."
+  :url "http://github.com/vedang/learns_stuff/little_cljer"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
