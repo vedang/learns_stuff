701383f4c717cd634074682ac101a3fda5ca893f
Vedang Manerikar <ved.manerikar@gmail.com>
2016-08-28 14:54:02 +0530

    update library dependencies

--- nuggets/project.clj
+++ nuggets/project.clj
@@ -2,5 +2,5 @@
   :description "tasty nuggets"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [enlive "1.1.5" :exclusions [org.clojure/clojure]]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha11"]
+                 [enlive "1.1.6" :exclusions [org.clojure/clojure]]])
