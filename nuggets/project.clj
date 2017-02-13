8345243d827ab70ab2d722b9a7751abc3ed8fd6a
Vedang Manerikar <ved.manerikar@gmail.com>
2017-02-13 23:42:02 +0530

    Add log4j2 to nuggets to make logging nicer
    This also gives me the chance to learn how to configure the excellent
    log4j2.

--- nuggets/project.clj
+++ nuggets/project.clj
@@ -1,6 +1,14 @@
-(defproject nuggets "0.1.0"
+(defproject nuggets "1.0.0"
   :description "tasty nuggets"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha11"]
-                 [enlive "1.1.6" :exclusions [org.clojure/clojure]]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [enlive "1.1.6" :exclusions [org.clojure/clojure]]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.apache.logging.log4j/log4j-api "2.8"]
+                 [org.apache.logging.log4j/log4j-core "2.8"]
+                 [org.apache.logging.log4j/log4j-slf4j-impl "2.8"]
+                 [com.fasterxml.jackson.core/jackson-core "2.8.5"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.8.5"]
+                 [com.fasterxml.jackson.core/jackson-annotations "2.8.5"]
+                 [org.jsoup/jsoup "1.10.2"]])
