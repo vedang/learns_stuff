3455df09c473ebad00db2c93778764fcd15259cf
Vedang Manerikar <ved.manerikar@gmail.com>
2017-03-12 15:49:03 +0530

    make-smart-array (Reentrant Lock)

--- joy/src/joy/ch10_mutation_and_concurrency_locks.clj
+++ joy/src/joy/ch10_mutation_and_concurrency_locks.clj
@@ -1,7 +1,8 @@
 (ns joy.ch10-mutation-and-concurrency-locks
   (:refer-clojure :exclude [aget aset count seq])
   (:require [clojure.core :as clj]
-            [joy.ch10-mutation-and-concurrency :refer (dothreads!)]))
+            [joy.ch10-mutation-and-concurrency :refer (dothreads!)])
+  (:import [java.util.concurrent.locks ReentrantLock]))
 
 
 (defprotocol SafeArray
@@ -46,3 +47,27 @@
   (def A (make-safe-array Integer/TYPE 8))
   (pummel A)
   (seq A))
+
+(defn lock-i
+  [target-index num-locks]
+  (mod target-index num-locks))
+
+(defn make-smart-array [t sz]
+  (let [a (make-array t sz)
+        lsz (/ sz 2)
+        L (into-array (take lsz (repeatedly #(ReentrantLock.))))]
+    (reify
+      SafeArray
+      (count [_] (clj/count a))
+      (seq   [_] (clj/seq a))
+      (aget  [_ i]
+        (let [lk (clj/aget L (lock-i (inc i) lsz))]
+          (.lock lk)
+          (try (clj/aget a i)
+               (finally (.unlock lk)))))
+      (aset  [this i f]
+        (let [lk (clj/aget L (lock-i (inc i) lsz))]
+          (.lock lk)
+          (try (clj/aset a i
+                         (f (aget this i)))
+               (finally (.unlock lk))))))))
