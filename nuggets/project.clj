19f9e9626ce24e763b76eed5329f20c567b46e0c
Vedang Manerikar <ved.manerikar@gmail.com>
2018-12-09 10:38:04 +0530

    Update project.clj deps to the latest versions

--- nuggets/project.clj
+++ nuggets/project.clj
@@ -2,14 +2,14 @@
   :description "tasty nuggets"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+  :dependencies [[org.clojure/clojure "1.10.0-RC4"]
                  [enlive "1.1.6" :exclusions [org.clojure/clojure]]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [org.apache.logging.log4j/log4j-api "2.8"]
-                 [org.apache.logging.log4j/log4j-core "2.8"]
-                 [org.apache.logging.log4j/log4j-slf4j-impl "2.8"]
-                 [com.fasterxml.jackson.core/jackson-core "2.8.5"]
-                 [com.fasterxml.jackson.core/jackson-databind "2.8.5"]
-                 [com.fasterxml.jackson.core/jackson-annotations "2.8.5"]
-                 [org.jsoup/jsoup "1.10.2"]
-                 [clj-time "0.13.0"]])
+                 [org.clojure/tools.logging "0.4.1"]
+                 [org.apache.logging.log4j/log4j-api "2.11.1"]
+                 [org.apache.logging.log4j/log4j-core "2.11.1"]
+                 [org.apache.logging.log4j/log4j-slf4j-impl "2.11.1"]
+                 [com.fasterxml.jackson.core/jackson-core "2.9.7"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.9.7"]
+                 [com.fasterxml.jackson.core/jackson-annotations "2.9.7"]
+                 [org.jsoup/jsoup "1.11.3"]
+                 [clj-time "0.15.1"]])
