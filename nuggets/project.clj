b295489050c6039ceb8146dd86758d9965f3ce11
Vedang Manerikar <ved.manerikar@gmail.com>
2020-01-06 09:40:21 +0530

    Update deps for nuggets

--- nuggets/project.clj
+++ nuggets/project.clj
@@ -2,14 +2,16 @@
   :description "tasty nuggets"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [enlive "1.1.6" :exclusions [org.clojure/clojure]]
-                 [org.clojure/tools.logging "0.4.1"]
-                 [org.apache.logging.log4j/log4j-api "2.11.1"]
-                 [org.apache.logging.log4j/log4j-core "2.11.1"]
-                 [org.apache.logging.log4j/log4j-slf4j-impl "2.11.1"]
-                 [com.fasterxml.jackson.core/jackson-core "2.9.7"]
-                 [com.fasterxml.jackson.core/jackson-databind "2.9.7"]
-                 [com.fasterxml.jackson.core/jackson-annotations "2.9.7"]
-                 [org.jsoup/jsoup "1.11.3"]
-                 [clj-time "0.15.1"]])
+                 [org.clojure/tools.logging "0.5.0"]
+                 [org.apache.logging.log4j/log4j-api "2.12.1"]
+                 [org.apache.logging.log4j/log4j-core "2.12.1"]
+                 [org.apache.logging.log4j/log4j-slf4j-impl "2.12.1"]
+                 [com.fasterxml.jackson.core/jackson-core "2.10.1"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.10.1"]
+                 [com.fasterxml.jackson.core/jackson-annotations "2.10.1"]
+                 [org.jsoup/jsoup "1.12.1"]
+                 [clj-time "0.15.2"]
+                 [clojure.java-time "0.3.2"]
+                 [org.clojure/test.check "0.10.0"]])
