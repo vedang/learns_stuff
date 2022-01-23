341598b243e43c11b3280723b8d7029a525668f9
Vedang Manerikar <ved.manerikar@gmail.com>
2022-01-23 20:02:27 +0530

    Update project dependencies to the latest versions

--- nuggets/project.clj
+++ nuggets/project.clj
@@ -2,14 +2,18 @@
   :description "tasty nuggets"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.2"]
+  :dependencies [[clj-time "0.15.2"]
+                 [clojure.java-time "0.3.3"]
+                 [com.brunobonacci/mulog "0.8.1"]
+                 [com.brunobonacci/mulog-json "0.8.1"]
+                 [com.brunobonacci/mulog-zipkin "0.8.1"]
                  [enlive "1.1.6" :exclusions [org.clojure/clojure]]
-                 [com.brunobonacci/mulog "0.6.4"]
-                 [com.brunobonacci/mulog-zipkin "0.6.4"]
-                 [com.brunobonacci/mulog-json "0.6.4"]
-                 [org.jsoup/jsoup "1.13.1"]
-                 [clj-time "0.15.2"]
-                 [clojure.java-time "0.3.2"]
-                 [org.clojure/test.check "1.1.0"]
-                 [org.clojure/core.async "1.3.610"]
-                 [instaparse "1.4.10"]])
+                 [hbs "1.0.3"]
+                 [hiccup "1.0.5"]
+                 [instaparse "1.4.10"]
+                 [org.clojure/clojure "1.10.3"]
+                 [org.clojure/core.async "1.5.648"]
+                 [org.clojure/test.check "1.1.1"]
+                 ;; Upgrading to 1.14 has breaking changes that need
+                 ;; to be fixed first.
+                 [org.jsoup/jsoup "1.13.1"]])
